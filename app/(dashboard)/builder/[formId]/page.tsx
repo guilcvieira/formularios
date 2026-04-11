df87cc174a0e35bec1a377721e8e5a8d76b8d9e9
Guilherme <guilcvieira@hotmail.com>
2026-04-11 01:33:07 -0300

    style: standardize quotes and formatting in BuilderPage component

--- app/(dashboard)/builder/[formId]/page.tsx
+++ app/(dashboard)/builder/[formId]/page.tsx
@@ -1,13 +1,17 @@
-import { GetFormById } from "@actions/form";
-import FormBuilder from "@/components/FormBuilder";
-import React from "react";
+import { GetFormById } from '@actions/form';
+import FormBuilder from '@/components/FormBuilder';
+import React from 'react';
 
-async function BuilderPage({ params }: { params: Promise<{ formId: string }> }) {
+async function BuilderPage({
+  params,
+}: {
+  params: Promise<{ formId: string }>;
+}) {
   const { formId } = await params;
   const form = await GetFormById(Number(formId));
 
   if (!form) {
-    throw new Error("Form not found");
+    throw new Error('Form not found');
   }
 
   return <FormBuilder form={form} />;
